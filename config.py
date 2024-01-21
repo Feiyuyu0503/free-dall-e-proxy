f67946a99c66785bdbda85e74a233eca15cee277
feiyuyu <stfailfy@gmail.com>
2024-01-21 21:46:45 +0800

    Add Discord configuration and requirements.txt

--- config.py
+++ config.py
@@ -2,10 +2,14 @@ from dotenv import load_dotenv
 import os
 
 class Config:
-    load_dotenv()  # Load .env file at the class level
+    load_dotenv(dotenv_path = os.path.join('data','.env'))  # Load .env file
 
     API_ID = int(os.getenv('API_ID'))
     API_HASH = os.getenv('API_HASH')
     BOT_USERNAME = os.getenv('BOT_USERNAME')
     SESSION_NAME = os.getenv('SESSION_NAME')
     FASTAPI_SERVER_URL = os.getenv('FASTAPI_SERVER_URL')
+
+    DiscordClientBotToken = os.getenv('DiscordClientBotToken')
+    DiscordChannelID = int(os.getenv('DiscordChannelID'))
+    DiscordDalleBotID = int(os.getenv('DiscordDalleBotId'))
