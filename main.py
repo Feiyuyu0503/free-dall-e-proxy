51f0841619e5fdc35be84b549de05ab0f4951d72
feiyuyu <stfailfy@gmail.com>
2024-01-31 22:35:29 +0800

    some minor changes

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ def setup_bots():
     # 创建一个字典，其中包含了不同平台的机器人客户端实例
     bot_clients = {}
 
-    if config.TELEGRAM == "True":
+    if config.TELEGRAM.lower() == "true":
         telegram_bot_client = TelegramBotClient(config.API_ID, config.API_HASH, config.BOT_USERNAME)
         bot_clients["telegram"] = telegram_bot_client
 
-    if config.Discord == "True":
+    if config.Discord.lower == "true":
         discord_bot_client = DiscordBotClient(config.DiscordClientBotToken, config.DiscordChannelID,config.DiscordDalleBotID)
         bot_clients["discord"] = discord_bot_client
     
