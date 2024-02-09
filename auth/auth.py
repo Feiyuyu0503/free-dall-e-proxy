2faaf71a0a3303e9805f35012b6f32ff53587058
feiyuyu <stfailfy@gmail.com>
2024-02-09 20:59:34 +0800

    Update API key validation messages

--- auth/auth.py
+++ auth/auth.py
@@ -14,7 +14,7 @@ def val_current_api_key(credentials: HTTPAuthorizationCredentials = Security(sec
     if config.Key and api_key not in config.Key:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="invalid api key. Go here to get your api key: https://dalle.feiyuyu.net/dashboard",
+            detail="invalid api key. Go here to get your api key: https://dalle.feiyuyu.net",
         )
     if api_key in config.keys["total_keys"].keys():
         user_id = list(config.keys["total_keys"][api_key].keys())[0]
@@ -22,7 +22,7 @@ def val_current_api_key(credentials: HTTPAuthorizationCredentials = Security(sec
         if curr_usage < 0:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="out of usage! Try again after reset.",
+                detail="out of usage! Try again after reset. Or deploy your own server without restrictions: https://github.com/Feiyuyu0503/free-dall-e-proxy",
             )
         else:
             config.keys["total_keys"][api_key][user_id][0] -= 1
