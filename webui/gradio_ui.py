576fb78f8d61321175bd2c0109b21ad7b67861d4
feiyuyu <stfailfy@gmail.com>
2024-07-13 14:40:49 +0800

    chore: add additional prompt

--- webui/gradio_ui.py
+++ webui/gradio_ui.py
@@ -52,6 +52,13 @@ def cancel():
 
 # 自定义 HTML 内容作为页脚
 footer_html = '''
+                <script type="text/javascript">
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "l86dzultnh");
+                </script>
                 <div id="footer">
                     <a href="/docs">API</a>
                      • 
