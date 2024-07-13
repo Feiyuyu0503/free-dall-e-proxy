c17dc932ce977ae7b217b48e1747e3af0c2ca103
feiyuyu <stfailfy@gmail.com>
2024-07-13 16:26:55 +0800

    chore: Update event listener for saving API key to local storage

--- webui/localStorage.js
+++ webui/localStorage.js
@@ -9,7 +9,7 @@ function saveApiKeyToLocalStorage() {
         inputElem.value = localStorage.getItem('api_key');
         inputElem.dispatchEvent(event);
     }
-    document.getElementById("component-7").addEventListener("click", function() {
+    document.getElementById("component-5").addEventListener("click", function() {
         var api_key = document.querySelector('input[data-testid="password"]').value
         if (api_key) {
             localStorage.setItem('api_key', api_key);
